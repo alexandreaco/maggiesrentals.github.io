bcd41a2c952d19bcee9e9e394a288115f980d566
Alexandrea Defreitas <alexandreadef@gmail.com>
2017-01-16 13:35:07 -0500

    Installs Flickity

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// Initialize Flickity
+var elem = document.getElementById('slideshow');
+if (elem) {
+  var flkty = new Flickity( elem, {
+    // options
+    cellAlign: 'left',
+    contain: true
+  });
+}
